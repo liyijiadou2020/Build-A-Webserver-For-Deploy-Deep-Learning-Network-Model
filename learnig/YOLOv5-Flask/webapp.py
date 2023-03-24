60c201d38b1905eda88f086970fd597bf353d5d0
liyijiadou2020 <liyijiadou@gmail.com>
2023-03-24 21:49:18 +0300

    Add docker-test

--- learnig/YOLOv5-Flask/webapp.py
+++ learnig/YOLOv5-Flask/webapp.py
@@ -18,10 +18,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def predict():
-    print(">> IN METHOD predict()...")
-
     if request.method == "POST":
-        print(">> RECEIVED POST ...")
         if "file" not in request.files:
             return redirect(request.url)
         file = request.files["file"]
@@ -41,8 +38,8 @@ def predict():
                 img = img[:, :, ::-1].transpose(2, 0, 1)
                 img = np.ascontiguousarray(img)
                 img = torch.from_numpy(img).to(device)
-                # img = img.half() if model.half() else img.float()  # uint8 to fp16/32 #half不支持
-                img = img.float()  # uint8 to fp16/32 #half不支持
+                # img = img.half() if model.half() else img.float()  # uint8 to fp16/32 #注意！CPU环境下请注释掉这句话
+                img = img.float()
                 img /= 255.0  # 0 - 255 to 0.0 - 1.0
                 if img.ndimension() == 3:
                     img = img.unsqueeze(0)
@@ -71,36 +68,12 @@ def predict():
     return render_template("index.html")
 
 
-# if __name__ == "__main__":
-#     weights = 'yolov5s.pt'
-#     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-#     print(">> DEBUG: device: ", device)
-#
-#     model = attempt_load(weights, map_location=device)
-#     model.to(device).eval()
-#     model.half()
-#     conf_thres = 0.25  # NMS置信度
-#     iou_thres = 0.45  # IOU阈值
-#     # 从模型中获取各类别名称
-#     names = model.module.names if hasattr(model, 'module') else model.names
-#     # 给每一个类别初始化颜色
-#     colors = [[random.randint(0, 255) for _ in range(3)] for _ in names]
-#     stride = int(model.stride.max())
-#     imgsz = check_img_size(640, s=stride)
-#
-#     app.run(debug=True, host="0.0.0.0", port=5000)  # debug=True causes Restarting with stat
-
+# YOLO model initialization
 weights = 'yolov5s.pt'
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-print(">> DEBUG: device: ", device)
-
+print(">> Your device: ", device)
 model = attempt_load(weights, map_location=device)
-print(">> attempt_load() finished!")
 model.to(device).eval()
-print(">> to(device).eval() finished!")
-
-# model.half() #TODO: 在CPU环境中half不可用！
-
 conf_thres = 0.25  # NMS置信度
 iou_thres = 0.45  # IOU阈值
 # 从模型中获取各类别名称
@@ -111,4 +84,5 @@ stride = int(model.stride.max())
 imgsz = check_img_size(640, s=stride)
 print(">> model set-up finished!")
 
+# web app
 app.run(debug=True, host="0.0.0.0", port=5000)  # debug=True causes Restarting with stat
