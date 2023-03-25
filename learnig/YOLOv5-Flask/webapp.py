4dae4c861c19c60be13dc890e8f48dae39accf21
liyijiadou2020 <liyijiadou@gmail.com>
2023-03-25 21:53:16 +0300

    fix bug: seperate GET and POST req

--- learnig/YOLOv5-Flask/webapp.py
+++ learnig/YOLOv5-Flask/webapp.py
@@ -16,55 +16,56 @@ from utils.plots import plot_one_box
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["POST"])
 def predict():
-    if request.method == "POST":
-        if "file" not in request.files:
-            return redirect(request.url)
-        file = request.files["file"]
-        if not file:
-            return
+    if "file" not in request.files:
+        return redirect(request.url)
+    file = request.files["file"]
+    if not file:
+        return
 
-        img_bytes = file.read()
-        img = Image.open(io.BytesIO(img_bytes))
-        img = cv2.cvtColor(np.asarray(img), cv2.COLOR_RGB2BGR)
+    img_bytes = file.read()
+    img = Image.open(io.BytesIO(img_bytes))
+    img = cv2.cvtColor(np.asarray(img), cv2.COLOR_RGB2BGR)
 
-        if img is not None:
-            showimg = img
-            with torch.no_grad():
-                img = letterbox(img, new_shape=imgsz)[0]
-                # Convert
-                # BGR to RGB, to 3x416x416
-                img = img[:, :, ::-1].transpose(2, 0, 1)
-                img = np.ascontiguousarray(img)
-                img = torch.from_numpy(img).to(device)
-                # img = img.half() if model.half() else img.float()  # uint8 to fp16/32 #注意！CPU环境下请注释掉这句话
-                img = img.float()
-                img /= 255.0  # 0 - 255 to 0.0 - 1.0
-                if img.ndimension() == 3:
-                    img = img.unsqueeze(0)
-                # Inference
-                pred = model(img)[0]
+    if img is not None:
+        showimg = img
+        with torch.no_grad():
+            img = letterbox(img, new_shape=imgsz)[0]
+            # Convert
+            # BGR to RGB, to 3x416x416
+            img = img[:, :, ::-1].transpose(2, 0, 1)
+            img = np.ascontiguousarray(img)
+            img = torch.from_numpy(img).to(device)
+            # img = img.half() if model.half() else img.float()  # uint8 to fp16/32 #注意！CPU环境下请注释掉这句话
+            img = img.float()
+            img /= 255.0  # 0 - 255 to 0.0 - 1.0
+            if img.ndimension() == 3:
+                img = img.unsqueeze(0)
+            # Inference
+            pred = model(img)[0]
 
-                # Apply NMS
-                pred = non_max_suppression(pred, conf_thres, iou_thres)
-                # Process detections
-                for i, det in enumerate(pred):  # detections per image
-                    if det is not None and len(det):
-                        # Rescale boxes from img_size to im0 size
-                        det[:, :4] = scale_coords(
-                            img.shape[2:], det[:, :4], showimg.shape).round()
-                        # Write results
-                        for *xyxy, conf, cls in reversed(det):
-                            label = '%s %.2f' % (names[int(cls)], conf)
-                            plot_one_box(
-                                xyxy, showimg, label=label, color=colors[int(cls)], line_thickness=2)
+            # Apply NMS
+            pred = non_max_suppression(pred, conf_thres, iou_thres)
+            # Process detections
+            for i, det in enumerate(pred):  # detections per image
+                if det is not None and len(det):
+                    # Rescale boxes from img_size to im0 size
+                    det[:, :4] = scale_coords(
+                        img.shape[2:], det[:, :4], showimg.shape).round()
+                    # Write results
+                    for *xyxy, conf, cls in reversed(det):
+                        label = '%s %.2f' % (names[int(cls)], conf)
+                        plot_one_box(
+                            xyxy, showimg, label=label, color=colors[int(cls)], line_thickness=2)
 
-        imgFile = "static/img.jpg"
-        cv2.imwrite(imgFile, showimg)
+    imgFile = "static/img.jpg"
+    cv2.imwrite(imgFile, showimg)
+    return redirect(imgFile)
 
-        return redirect(imgFile)
 
+@app.route("/", methods=["GET"])
+def index():
     return render_template("index.html")
 
 
